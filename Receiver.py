612c2ce5228c8ca265a8cd9b7d455b16ce48f62a
wgaylord <chibill110@gmail.com>
2019-09-29 00:36:53 -0500

    Add audio

---
+++ Receiver.py
@@ -0,0 +1,30 @@
+import zmq
+import pyaudio
+from cv2 import VideoCapture
+import cv2
+import numpy
+import struct
+
+ip = "127.0.0.1"
+port = 8000
+
+
+PyAudio = pyaudio.PyAudio()
+stream = PyAudio.open(2048*8,1,pyaudio.paInt16,False,True,None,None,2048,True,None,None)
+context = zmq.Context()
+SubscribeSocket = context.socket(zmq.SUB)
+SubscribeSocket.connect("tcp://"+ip+":%s" % port)
+SubscribeSocket.setsockopt(zmq.SUBSCRIBE, b"")
+       
+while True:
+    buffer = SubscribeSocket.recv()
+    #print(buffer[0])
+    if buffer[0] == b"A":
+        stream.write(buffer[1:])
+        continue
+    shape = struct.unpack("HH",buffer[1:5])
+    frameBytes = buffer[5:]
+    frame = numpy.frombuffer(frameBytes,dtype = numpy.uint8).reshape((shape[0],shape[1],3))
+    cv2.imshow("Video",frame)
+    cv2.waitKey(1)
+   
---
+++ Sender.py
@@ -0,0 +1,40 @@
+import zmq
+import pyaudio
+from cv2 import VideoCapture
+import cv2
+import numpy
+import struct
+
+port = 8000
+camNumber = 0
+
+PyAudio = pyaudio.PyAudio()
+context = zmq.Context()
+PublishSocket = context.socket(zmq.PUB)
+PublishSocket.set_hwm(2)
+PublishSocket.bind("tcp://0.0.0.0:%s" % port)
+capture = VideoCapture(camNumber)
+
+
+   
+def Audio(in_data,frame_count,time_info,status_flag):
+    PublishSocket.send(b"A"+in_data) 
+    return (None,0)
+
+stream = PyAudio.open(2048*8,1,pyaudio.paInt16,True,False,None,None,2048,True,None,None,Audio)       
+    
+stream.start_stream()
+
+old_frame = capture.read()
+while True:
+    buffer = b"V"
+    ret,frame = capture.read()
+    if numpy.array_equal(old_frame,frame):
+        cv2.waitKey(1)
+        continue
+    frameBytes = frame.tobytes()
+    buffer += struct.pack("HH",frame.shape[0],frame.shape[1])
+    buffer += frameBytes
+    PublishSocket.send(buffer)
+    old_frame = frame
+    cv2.waitKey(1)
